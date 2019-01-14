8f7db81b75a09959d9a2bdf6ec77048939d8665f
Fabian Flores <niir.rff2@gmail.com>
2019-01-14 16:12:59 -0600

    Ajuste de Activiy de Operador

--- app/src/main/java/jac/infosyst/proyectogas/vista/SplashActivity.java
+++ app/src/main/java/jac/infosyst/proyectogas/vista/SplashActivity.java
@@ -21,7 +21,7 @@ public class SplashActivity extends Activity {
         {
             @Override
             public void run() {
-                Intent intent = new Intent(SplashActivity.this, LoginActivity.class);
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
                 startActivity(intent);
             }
 
